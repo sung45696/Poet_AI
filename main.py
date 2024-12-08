e2f10a47667d4289314fa0cd3b3aae4b551284c6
sung45696 <sung78787878@gmail.com>
2024-12-08 20:59:03 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #pip install python-dotenv
 #pip install langchain-openai
 #pip install streamlit
-
-# from dotenv import load_dotenv
-# load_dotenv()
-
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
 
